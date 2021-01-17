af04339d67beae4e26ef518fbdb45f8229f4919d
tansonlee <leetanson@gmail.com>
2021-01-17 17:05:51 -0500

    homepage project cards adjust to size of screen

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,13 +1,53 @@
 import "./Projects.css";
+import ProjectCard from "../ProjectCard";
+import {
+	MachineLanguage,
+	SortingAlgorithms,
+	LindenmayerSystems,
+	SudokuSolver,
+	Boid,
+	BST,
+	FunctionalList,
+	RandomData,
+} from "../../project-data/data";
 
 function Projects() {
 	return (
 		<>
-			<h1>Project Page!</h1>
-			<h2>Running app</h2>
-			<h2>Machine Language</h2>
-			<h2>Sorting Algorithms</h2>
-			<h2>Sudoku Solver</h2>
+			<h1 className="projects-header">All Projects!</h1>
+
+			<div className="projects-description-wrapper">
+				<p className="projects-description">
+					All projects can be found on my GitHub here:{" "}
+					<a
+						className="projects-github-link"
+						href="https://github.com/tansonlee"
+					>
+						GitHub
+					</a>
+				</p>
+			</div>
+
+			<div className="projects-project-card-wrapper-wrapper">
+				<div className="projects-project-card-wrapper">
+					<ProjectCard {...MachineLanguage} />
+					<ProjectCard {...SortingAlgorithms} />
+					<ProjectCard {...LindenmayerSystems} />
+					<ProjectCard {...SudokuSolver} />
+					<ProjectCard {...Boid} />
+					<ProjectCard {...BST} />
+					<ProjectCard {...FunctionalList} />
+					<ProjectCard {...RandomData} />
+					<ProjectCard {...MachineLanguage} />
+					<ProjectCard {...SortingAlgorithms} />
+					<ProjectCard {...LindenmayerSystems} />
+					<ProjectCard {...SudokuSolver} />
+					<ProjectCard {...Boid} />
+					<ProjectCard {...BST} />
+					<ProjectCard {...FunctionalList} />
+					<ProjectCard {...RandomData} />
+				</div>
+			</div>
 		</>
 	);
 }
