ce2f1976e9faf33ce23edfd1052a0e1a40211876
tansonlee <leetanson@gmail.com>
2021-04-09 13:35:31 -0400

    added form submission in back end

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -6,23 +6,17 @@ const AboutMe = () => {
 		<>
 			<h1 className="page-header">About Me</h1>
 			<p className="self-description card-shadow">
-				I'm a Computer Science student at the University of Waterloo. I
-				built this website as an accessible way to showcase some of my
-				projects.
+				I'm a Computer Science student at the University of Waterloo. I built this website as an accessible way to showcase some of my projects.
 			</p>
 			<div className="skills-wrapper">
 				{SkillData.map(({ topic, items }) => (
-					<div className="skill-card">
+					<div key={topic} className="skill-card">
 						<h3 className="skill-topic">{topic}</h3>
 						<ul className="skill-list">
 							{items.map(({ item, image }) => (
 								<li className="skill-item">
 									<span className="skill-span">
-										<img
-											src={image}
-											alt={item}
-											className="skill-image"
-										/>
+										<img src={image} alt={item} className="skill-image" />
 									</span>
 									<p>{item}</p>
 								</li>
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,44 +1,66 @@
 import "./ContactPage.css";
 import emailjs from "emailjs-com";
 import { useState } from "react";
+import axios from "axios";
 
 const Contact = () => {
 	const [hasSubmitted, setHasSubmitted] = useState(false);
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		subject: "",
+		message: "",
+	});
 
+	// const handleSubmit = e => {
+	// 	e.preventDefault(); // Prevents default refresh by the browser
+	// 	emailjs
+	// 		.sendForm(
+	// 			process.env.REACT_APP_SERVICE_ID,
+	// 			process.env.REACT_APP_TEMPLATE_ID,
+	// 			e.target,
+	// 			process.env.REACT_APP_USER_ID
+	// 		)
+	// 		.then(
+	// 			result => {
+	// 				// alert(
+	// 				// 	"Message Sent, I'll get back to you shortly",
+	// 				// 	result.text
+	// 				// );
+	// 				setHasSubmitted(true);
+	// 			},
+	// 			error => {
+	// 				alert("An error occured, Plese try again", error.text);
+	// 				console.log(error);
+	// 			}
+	// 		);
+	// };
 	const handleSubmit = e => {
 		e.preventDefault(); // Prevents default refresh by the browser
-		emailjs
-			.sendForm(
-				process.env.REACT_APP_SERVICE_ID,
-				process.env.REACT_APP_TEMPLATE_ID,
-				e.target,
-				process.env.REACT_APP_USER_ID
-			)
-			.then(
-				result => {
-					// alert(
-					// 	"Message Sent, I'll get back to you shortly",
-					// 	result.text
-					// );
-					setHasSubmitted(true);
-				},
-				error => {
-					alert("An error occured, Plese try again", error.text);
-					console.log(error);
-				}
-			);
+
+		console.log(formData);
+		axios({
+			method: "post",
+			url: "https://tansonlee-emailer.herokuapp.com/",
+			data: {
+				subject: formData.subject,
+				body: `NAME: ${formData.name}\nSUBJECT: ${formData.subject}\nEMAIL: ${formData.name}\n\nMESSAGE:\n${formData.message}`,
+			},
+		}).then(
+			response => {
+				setHasSubmitted(true);
+				console.log(response);
+			},
+			error => {
+				console.log(error);
+			}
+		);
 	};
 
 	return (
 		<>
 			<h1 className="page-header">Contact Me!</h1>
-			<form
-				name="contact-form"
-				method="POST"
-				action="POST"
-				className="contact-form"
-				onSubmit={handleSubmit}
-			>
+			<form name="contact-form" method="POST" action="POST" className="contact-form" onSubmit={handleSubmit}>
 				<input type="hidden" name="contact-form" value="contact-form" />
 				<div className="small-input-warpper">
 					<p className="prompt">Your Name:</p>
@@ -47,6 +69,7 @@ const Contact = () => {
 						type="text"
 						placeholder="John Smith"
 						className="small-input"
+						onChange={e => setFormData({ ...formData, name: e.target.value })}
 					/>
 					<p className="prompt">Your Email:</p>
 					<input
@@ -55,6 +78,7 @@ const Contact = () => {
 						type="text"
 						placeholder="email@email.com"
 						className="small-input"
+						onChange={e => setFormData({ ...formData, email: e.target.value })}
 					/>
 					<p className="prompt">Subject:</p>
 					<input
@@ -62,6 +86,7 @@ const Contact = () => {
 						type="text"
 						placeholder="Subject . . ."
 						className="small-input"
+						onChange={e => setFormData({ ...formData, subject: e.target.value })}
 					/>
 				</div>
 				<div className="large-input-wrapper">
@@ -73,6 +98,7 @@ const Contact = () => {
 							type="text"
 							placeholder="Message . . ."
 							className="large-input"
+							onChange={e => setFormData({ ...formData, message: e.target.value })}
 						></textarea>
 					</div>
 					<button type="submit" className="form-submit">
@@ -80,9 +106,7 @@ const Contact = () => {
 					</button>
 				</div>
 			</form>
-			{hasSubmitted ? (
-				<p className="submitted-message">Thanks for submitting!</p>
-			) : null}
+			{hasSubmitted ? <p className="submitted-message">Thanks for submitting!</p> : null}
 		</>
 	);
 };
